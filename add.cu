2970938a291468f91c9deb7353d9a3daf5dbda8a
Bojan Tunguz <tunguz@gmail.com>
2025-11-17 22:44:20 +0000

    add cu parallelism

--- add.cu
+++ add.cu
@@ -5,7 +5,9 @@
 __global__
 void add(int n, float *x, float *y)
 {
- for (int i = 0; i < n; i++)
+  int index = threadIdx.x;
+  int stride = blockDim.x; 
+  for (int i = index; i < n; i += stride)
    y[i] = x[i] + y[i];
 }
  
@@ -25,7 +27,7 @@ int main(void)
  }
  
  // Run kernel on 1M elements on the GPU
- add<<<1, 1>>>(N, x, y);
+ add<<<1, 256>>>(N, x, y);
  
  // Wait for GPU to finish before accessing on host
  cudaDeviceSynchronize();
